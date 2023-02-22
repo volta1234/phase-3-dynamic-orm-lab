6432a831cddf57b57439c95d1e596eb8b60a965b
CALVINCE <calvince.ogwel@gmail.com>
2023-02-22 23:00:05 +0300

    completed lab

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,24 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
+require 'sqlite3'
+
 class InteractiveRecord
-  
+
+  def self.table_name
+    # returns the table name associated with the class
+  end
+
+  def self.column_names
+    # returns an array of column names associated with the class
+  end
+
+  def initialize(attributes={})
+    # initializes a new instance of the class with the given attributes
+  end
+
+  def save
+    # saves the instance to the database
+  end
+
 end
